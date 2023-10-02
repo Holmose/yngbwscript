a0174b365366069d9aaf8f2aa1e17f7eb30007a6
holmose <holmose@qq.com>
2023-10-02 16:53:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+### 使用说明
+需要首先安装aibote
+下载地址：http://www.aibote.net/download.html
+
+python版本为：3.10.13
+
+安装依赖
+```bash
+pip install -r requrements.txt
+```
+
+修改用户名和密码
+```python
+USERNAME = "用户名"
+PASSWORD = "密码"
+```
+
+需要安装官方版本的chrome浏览器
+
+然后启动运行
+```bash
+python web_bot.py
+```
---
+++ web_bot.py
@@ -0,0 +1,264 @@
+#-*- coding: utf8 -*-
+import time
+import base64
+import ddddocr
+import sys
+import re
+import os
+import lxml
+from bs4 import BeautifulSoup
+
+
+# 1. 导入 WebBotMain 类
+from AiBot import WebBotMain
+
+USERNAME = "用户名"
+PASSWORD = "密码"
+
+# 2. 自定义一个脚本类，继承 WebBotMain
+class CustomWebScript(WebBotMain):
+    # 3. 设置等待参数
+    # 3.1 设置等待时间
+    wait_timeout = 3
+    # 3.2 设置重试间隔时长
+    interval_timeout = 0.5
+
+    # 4. 设置日志等级
+    log_level = "INFO"  # "DEBUG"
+
+    # 5. 设置方法超时是否抛出异常
+    raise_err = False  # True
+
+    # 定义OCR识别
+    ocr = ddddocr.DdddOcr()
+
+    # 今日未学个数
+    unlearned = 0
+
+    # 今日总学分数
+    total_core = 10
+
+    def close_all(self):
+        self.quit()
+        sys.exit(1)
+
+    # 6. 重写方法，编写脚本
+    # 注意：此方法是脚本执行入口
+    def script_main(self):
+        # 6. API 演示
+        # 注意：Python 版本支持的 Api 与 Nodejs 基本相同
+        # 教程中仅演示部分 Api，更多 Api 请自行探索，所有 Api 均包含详细的参数要求和返回值，请自行查看。
+
+        # 打开主页
+        self.goto("https://www.ynsgbzx.cn/index.aspx")
+        home_page_id = self.get_current_page_id()
+
+        while True:
+            try:
+                time.sleep(self.wait_timeout)
+                if self.get_element_text('//*[@id="go"]') == '退出系统':
+                    print("登录成功！")
+                    break
+                # 清空输入框
+                self.clear_element('//*[@id="LoginView1_Login1_UserName"]')
+                self.clear_element('//*[@id="LoginView1_Login1_Password"]')
+                self.clear_element('//*[@id="LoginView1_Login1_txtValidate"]')
+
+                self.send_keys('//*[@id="LoginView1_Login1_UserName"]', USERNAME)
+                self.send_keys('//*[@id="LoginView1_Login1_Password"]', PASSWORD)
+
+                # 获取登录验证码
+                check_code = self.image_check('//*[@id="ImageCheck"]')
+                print('识别出的验证码为：' + check_code)
+                # 填入验证码
+                self.send_keys('//*[@id="LoginView1_Login1_txtValidate"]', check_code)
+
+                # 点击登录
+                self.click_element('//*[@id="LoginView1_Login1_LoginButton"]')
+                time.sleep(self.wait_timeout)
+
+                if self.click_alert(True):
+                    print("点击确定!")
+                    time.sleep(self.wait_timeout)
+                    
+            except Exception as e:
+                print("出现错误，等待 %s %ss" % (e, self.wait_timeout))
+                time.sleep(self.wait_timeout)
+
+        # 点击个人空间
+        time.sleep(self.wait_timeout)
+        print('点击个人空间')
+        self.click_element('//*[@id="navbar"]/ul[1]/li[6]/a')
+        time.sleep(self.wait_timeout)
+
+        page_message = self.get_element_text('//*[@id="MyCourseList1_AspNetPager1"]/div[1]')
+        total_page = re.findall(r'共(\d+)页', page_message)[0]
+        print("一共有", total_page, "页", "，课件数：", re.findall(r'课件数：(\d+)', page_message)[0])
+
+        message = self.get_element_text('//*[@id="collapseone"]')
+        print()
+        print("-"*10)
+        print(message)
+        print("-"*10)
+        print()
+
+        # 获取学习分数
+        mark_message = self.get_element_text('//*[@id="form1"]/div[7]/div/div[2]/div[1]/div/div[2]/p[4]/mark')
+        learned = re.findall(r'(\d+\.\d+)分', mark_message)
+        if learned:
+            print('已经学习：%s分！' % learned[0])
+            if float(learned[0])>=self.total_core:
+                print('今日分数已学满！')
+                self.close_all()
+            else:
+                print('今日分数未满，开始学习！')
+                self.unlearned = self.total_core - float(learned[0])
+        
+        for batch in range(int(total_page)):
+            print("开始学习第", batch+1, "页！")
+            
+            self.extract_url()
+
+            time.sleep(self.wait_timeout)
+            self.switch_to_page(home_page_id)
+            time.sleep(self.wait_timeout)
+            self.click_element('//*[@id="MyCourseList1_AspNetPager1"]/div[2]/a[last()]')
+            time.sleep(self.wait_timeout)
+        
+        print("今日学习完成！")
+        self.close_all()
+
+
+    # 验证码识别
+    def image_check(self, xpath:str) -> str:
+        # 截屏验证码
+        image_base64 = self.save_screenshot(xpath)
+        # 解码图片
+        img_bytes = base64.b64decode(image_base64)
+
+        # 识别验证码
+        check_code = self.ocr.classification(img_bytes)
+        
+        return check_code
+    
+    # 获取每页的链接开始观看
+    def extract_url(self):
+        outer_html = self.get_element_outer_html('//*[@id="form1"]/div[7]/div/div[2]/div[2]/div[3]/div/div[1]/table/tbody')
+        soup = BeautifulSoup(outer_html,'lxml')
+        link_titles = soup.select('.yx-common-link')
+        process = soup.select('.progress-bar-warning')
+        for i,v in enumerate(process):
+            temp = v.text.strip()
+            if temp == '100%':
+                continue
+            else:
+                print("开始学习：", link_titles[i].text.strip())
+                course_id = re.findall('id=(\d+)', link_titles[i]['href'])[0]
+
+                url = 'https://www.ynsgbzx.cn/play/play.aspx?course_id=' + course_id
+                self.watch_video(link_titles[i].text.strip(), url)
+                self.unlearned -= 1
+                print("当前分数：%s" % (self.total_core - self.unlearned))
+                if (self.unlearned<=0):
+                    print("今日分数已学满！")
+                    self.close_all()
+                print("开始播放下一个视频！")
+                time.sleep(self.wait_timeout*2)
+
+    # 观看每个视频
+    def watch_video(self,title, url):
+        print("开始学习：%s" % title)
+        self.new_page(url)
+        time.sleep(self.wait_timeout)
+
+        while True:
+            # 识别验证码
+            check_code = self.image_check('//*[@id="ImageCheck"]')
+            self.clear_element('//*[@id="validanswer"]')
+            print('识别出的验证码为：' + check_code)
+            self.send_keys('//*[@id="validanswer"]', check_code)
+
+            # 点击登录
+            self.click_element('//*[@id="btnvalidanswer"]')
+            time.sleep(self.wait_timeout)
+            if self.click_alert(True):
+                print("验证码错误!")
+                time.sleep(self.wait_timeout)
+            else:
+                print("验证码正确")
+                break
+
+        time.sleep(self.wait_timeout)
+        self.switch_to_frame('//*[@id="ifr"]')
+        self.switch_to_frame('//*[@id="mainFrame"]')
+
+        # 获取未看时间点
+        unwatch_images = self.get_element_text('//*[@id="lnode"]')
+        # 未学时间点：1-602-14,16-25,27-32,34-41,43-50,52-54分钟
+        # 未学时间点：1-1203-7,9-20,22-32,34-40,42-43分钟
+        # 未学时间点：7-14,16-46,50-50分钟
+        # 未学时间点：46-73分钟
+        unwatch = re.findall(r'点：(\d+)', unwatch_images)
+        if unwatch:
+            # 计算秒数
+            time_point = (int(unwatch[0])-1) * 60
+            print("检测到未看时间点，跳转到 %s 秒" % time_point)
+            # 跳转到未看时间点
+            self.execute_script("document.querySelector('video').currentTime=%s;" % time_point)                
+
+        while True:
+            try:
+                time.sleep(2)
+                # 总共时长
+                total_time = self.execute_script("document.querySelector('video').duration;")
+                if total_time:
+                    total_time = float(total_time)
+                # 实时监听进度
+                curent_time = self.execute_script("document.querySelector('video').currentTime;")
+                if curent_time:
+                    curent_time = float(curent_time)
+
+                total_iterations = 100
+                temp_iter = (curent_time / total_time) * 100
+                width = os.get_terminal_size().columns
+                self.print_progress_bar(iteration=temp_iter, total=total_iterations, prefix='进度:', suffix='完成', length=50)
+
+                if total_time - 4 <= curent_time:
+                    print("播放结束！")
+                    self.switch_to_frame('//*[@id="ifr"]')
+                    self.switch_to_main_frame()
+                    time.sleep(6)
+                    print("关闭当前页面！")
+                    self.close_current_page()
+                    break
+            except Exception as e:
+                print("出现错误，等待 %s %ss" % (e, self.wait_timeout))
+                time.sleep(self.wait_timeout)
+        
+    def print_progress_bar(self, iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█'):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '-' * (length - filled_length)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
+        # Print a new line when progress is complete
+        if iteration == total:
+            print()
+
+
+# 7. 执行脚本，Pycharm 中，直接右键执行
+if __name__ == '__main__':
+    # 启动脚本，监听 9999 号端口
+    # 默认使用 Chrome 浏览器
+
+    # local=True 时，是本地运行脚本，会自动启动 WebDriver.exe 驱动；
+    # 在远端部署脚本时，请设置 local=False，手动启动 WebDriver.exe，启动 WebDriver.exe 时需指定远端 IP 或端口号；
+
+    # 如本地部署脚本，需要传递 WebDriver 启动参数时，参考下面方式，如不需传递启动参数，则忽略：
+    driver_params = {
+        "browserName": "chrome",
+        "debugPort": 0,
+        "userDataDir": "./UserData",
+        "browserPath": None,
+        "argument": None,
+    }
+    CustomWebScript.execute(9999, local=True, driver_params=driver_params)
